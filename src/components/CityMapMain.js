03327dc5c1a608d3a3de3e0257542632676f2920
Omar Faleh <omar@morscad.com>
2020-04-05 16:24:26 -0400

    Use Links to navigate between pages instead of js locations

--- src/components/CityMapMain.js
+++ src/components/CityMapMain.js
@@ -1,13 +1,15 @@
 import React from "react";
+import {
+    Link
+} from "react-router-dom";
+
 import "./CityMapMain.scss";
 
 function CityMapMain() {
   return (
     <div className="mainContainer">
       <div>This is an Intro</div>
-      <div onClick={() => {
-        window.location = './map'
-      }}>Enter</div>
+      <Link to={"/map"}>Enter</Link>
     </div>
   );
 }
