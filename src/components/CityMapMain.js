d441931853b7d8f320da28da5c5fce3234d3981a
Omar Faleh <omar@morscad.com>
2020-04-12 15:23:03 -0400

    skin homepage

--- src/components/CityMapMain.js
+++ src/components/CityMapMain.js
@@ -4,12 +4,25 @@ import { Link } from "react-router-dom";
 import "./CityMapMain.scss";
 import MainLayout from "../layout/MainLayout";
 
+import homepageLogo from "../assets/images/logo-main.png";
+
 function CityMapMain() {
   return (
     <MainLayout>
       <div className="mainContainer">
-        <div>This is an Intro</div>
-        <Link to={"/map"}>Enter</Link>
+        <div className={"homepageLogoContainer"}>
+          <div className={"homepageLogo"}>
+            <div className={"logo"}>
+              <img src={homepageLogo} />
+            </div>
+            <div className={"link"}>
+              <Link to={"/map"}>enter &raquo;</Link>
+            </div>
+          </div>
+          <div className={"logoFooter"}>
+            A New York University Research Project
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
