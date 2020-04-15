582989dbf9f6ae0cd09c49f77e2c0285e98a9f99
Omar Faleh <omar@morscad.com>
2020-04-15 11:29:29 -0400

    Blank page containers

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MainLayout from "../layout/MainLayout";
+import './About.scss';
+
+const About = () => {
+    return (
+        <MainLayout>
+            <div>
+                About
+            </div>
+        </MainLayout>
+    )
+}
+
+export default About;
---
+++ src/components/HowItWorks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import MainLayout from "../layout/MainLayout";
+import './HowItWorks.scss';
+
+const HowItWorks = () => {
+    return (
+        <MainLayout>
+            <div>
+                How It Works
+            </div>
+        </MainLayout>
+    );
+}
+
+export default HowItWorks;
