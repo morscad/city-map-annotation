e337585a1e41074155d7d36ede8691b6bb80bbb5
Omar Faleh <omar@morscad.com>
2020-04-15 11:29:12 -0400

    Add missing router entries, load the google scripts at entry point

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import CityMapMain from "./components/CityMapMain";
-import MapContainer from "./components/MapContainer";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CityMapMain from './components/CityMapMain';
+import MapContainer from './components/MapContainer';
+import { LoadScript } from '@react-google-maps/api';
+import About from "./components/About";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <CityMapMain />
-        </Route>
-        <Route exact path="/map">
-          <MapContainer />
-        </Route>
-      </Switch>
-    </Router>
+    <LoadScript id="google-maps-loader" googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} libraries={['places']}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <CityMapMain />
+          </Route>
+          <Route exact path="/map">
+            <MapContainer />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/how-it-works">
+            <HowItWorks />
+          </Route>
+        </Switch>
+      </Router>
+    </LoadScript>
   );
 };
 
