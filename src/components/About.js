11511e6e2320644e701f927cc3a5fe4547a27ac8
Omar Faleh <omar@morscad.com>
2020-05-02 07:55:31 -0400

    add static page content

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, {useState} from 'react';
+import { Redirect } from 'react-router-dom';
 
-import MainLayout from "../layout/MainLayout";
+import MainLayout from '../layout/MainLayout';
 import './About.scss';
 
 const About = () => {
-    return (
-        <MainLayout>
-            <div>
-                About
-            </div>
-        </MainLayout>
-    )
-}
+  const [allowReroute, setAllowReroute] = useState(false);
+  return (
+    <MainLayout>
+      {allowReroute && <Redirect to="/map" />}
+      <div className={'videoContainer'}>
+        <iframe
+          src="https://player.vimeo.com/video/413972673"
+          width="1200"
+          height="675"
+          frameBorder="0"
+          allow="autoplay; fullscreen"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className={'buttonContainer'}>
+        <div className={'button'} onClick={() => { setAllowReroute(true)}}>Go to the map</div>
+      </div>
+      <div className={'contentContainer'}>
+        <div>
+          City Data Stories is a multi-phase research project for creating a digital platform that allows users to co-create
+          multi-sensory geographies and contribute to the ongoing narrative of their city.
+        </div>
+
+        <div>
+          Users can annotate the city with visual and oral stories by using a web platform that leverages Google Maps to allow
+          people to upload their media to a specific location or address.
+        </div>
+
+        <div>
+          The result is a location-specific crowdsourced database of media and texts, which will later be used to create a
+          mediated augmented reality urban walk that brings viewers on a journey that tells the story of the city through data.
+        </div>
+
+        <div>
+          On top of user-generated content, City Data Stories uses data from two other source:
+          <ul>
+            <li>
+              The Opportunity Atlas project, which was created by researchers at Harvard University, Brown University, and the
+              United States Census Bureau;
+            </li>
+            <li>A crowdsourced database of urban tags from the Hoodmaps website;</li>
+          </ul>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
 
 export default About;
--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
-
-import MainLayout from "../layout/MainLayout";
+import MainLayout from '../layout/MainLayout';
 import './HowItWorks.scss';
 
 const HowItWorks = () => {
-    return (
-        <MainLayout>
-            <div>
-                How It Works
-            </div>
-        </MainLayout>
-    );
-}
+  return (
+    <MainLayout>
+      <div className={'instructionsContainer'}>
+        <div>
+          Upon entering the website, users can navigate the city map, search for addresses and landmarks, zoom in and out the map,
+          and watch their map update with geo-specific tags and images.
+        </div>
+
+        <div>
+          They can also choose what data to display by toggling visibility by type or by data-source, and navigate the details of
+          the embedded media and read their captions.
+        </div>
+
+        <div>
+          {' '}
+          City Data Stories uses a reporting system for identifying offensive and problematic content to be removed from the
+          database. Future iterations of this platform will also include a voting system and comments.
+        </div>
+
+        <div>
+          Users can choose to add their own annotations to the city map by clicking on “Add Yours”, choosing a location by
+          searching or by navigating the map, and clicking on the area they wish to annotate to add their media and captions.
+        </div>
+
+        <div>Users can choose to remain anonymous or enter their name when uploading the media.</div>
+
+        <div>
+          {' '}
+          Once completed, the mediated augmented reality urban walk will load annotations by proximity and use them to overlay the
+          existing urban environment.
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
 
 export default HowItWorks;
